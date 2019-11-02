26a8a4abfdfd1e990fb1ce57df35215fc23c888c
Alex <alex@apresnail.com>
2019-11-02 13:32:45 -0700

    testing

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express();
+
+
+
+
+import bodyParser from 'body-parser'
+import express from 'express'
+const app = express()
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+const router = express.Router()
+
+router.get('/cities', (req, res) => {
+  const cities = [
+    {name: 'New York City', population: 8175133},
+    {name: 'Los Angeles',   population: 3792621},
+    {name: 'Chicago',       population: 2695598}
+  ]
+  res.json(cities)
+})
+
+app.use(router)
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
